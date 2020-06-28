362f2a6c29da54d24dc6b5bd62bff92d5b81d9cb
joselejinpj <67502523+joselejinpj@users.noreply.github.com>
2020-06-28 21:32:04 +0530

    Create GameOfLifeBoard.java

---
+++ Java/GameOfLifeBoard.java
@@ -0,0 +1,78 @@
+/*****
+
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0). 
+Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules:
+
+Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population..
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+Write a function to compute the next state after one update of the board given its current state. 
+The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
+
+-----------------
+Sample Test Cases
+-----------------
+Test Case 1:
+
+Input: 
+[
+  [0,1,0],
+  [0,0,1],
+  [1,1,1],
+  [0,0,0]
+]
+
+Output: 
+[
+  [0,0,0],
+  [1,0,1],
+  [0,1,1],
+  [0,1,0]
+]
+
+*****/
+
+class Solution {
+    public void gameOfLifeBoard(int[][] board) {
+        int[] neighbors = {0, 1, -1};
+        int rows = board.length;
+        int cols = board[0].length;
+        int[][] copyBoard = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // The evaluation is done against the copy
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                if ((copyBoard[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = 0;
+                }
+
+                if (copyBoard[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 1;
+                }
+            }
+        }
+    }
+}
