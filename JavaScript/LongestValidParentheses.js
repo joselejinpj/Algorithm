b06506398eed221ec22993960d8c7758be9bd1d1
joselejinpj <67502523+joselejinpj@users.noreply.github.com>
2020-06-30 22:58:50 +0530

    Create LongestValidParentheses.js

---
+++ JavaScript/LongestValidParentheses.js
@@ -0,0 +1,48 @@
+/*****
+
+Given a string containing just the characters '(' and ')', find the length of the longest valid parentheses substring.
+
+-----------------
+Sample Test Cases
+-----------------
+Test Case 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+
+Test Case 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+
+*****/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    var stack = [];
+    var longestValid = 0;
+    
+    stack.push(-1);
+    
+    for(var i=0; i<s.length; i++) {
+        var currentChar = s.charAt(i);
+        
+        if(currentChar == "(")
+            stack.push(i)
+        else {
+            stack.pop();
+            
+            if(stack.length == 0) 
+                stack.push(i)
+            else
+                longestValid = Math.max(longestValid, i-stack[stack.length-1]);
+        }
+    }
+    
+    return longestValid;
+};
