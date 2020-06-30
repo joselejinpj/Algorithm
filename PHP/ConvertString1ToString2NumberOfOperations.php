32deacbc3776af3441eb4c8b5fb26e34d94c0e40
joselejinpj <67502523+joselejinpj@users.noreply.github.com>
2020-07-01 01:02:28 +0530

    Update ConvertString1ToString2NumberOfOperations.php

--- PHP/ConvertString1ToString2NumberOfOperations.php
+++ PHP/ConvertString1ToString2NumberOfOperations.php
@@ -1,8 +1,8 @@
 /*****
 
-Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+Given two strings string1 and string2, find the minimum number of operations required to convert string1 to string2.
 
-Following 3 operations are allowed on a word:
+Following 3 operations are allowed on a string:
 
 Insert a character
 Delete a character
@@ -14,7 +14,7 @@ Sample Test Cases
 
 Test Case 1:
 
-Input: word1 = "horse", word2 = "ros"
+Input: string1 = "horse", string2 = "ros"
 Output: 3
 Explanation: 
 horse -> rorse (replace 'h' with 'r')
@@ -23,7 +23,7 @@ rose -> ros (remove 'e')
 
 Test Case 2:
 
-Input: word1 = "intention", word2 = "execution"
+Input: string1 = "intention", string2 = "execution"
 Output: 5
 Explanation: 
 intention -> inention (remove 't')
@@ -40,25 +40,26 @@ exection -> execution (insert 'u')
  * @return Integer
  */
 <?php 
+
 function minDistance($str1, $str2, $m, $n) {  
-    // dp[][] in bottom up manner  - Dynamic programming memoization
-	  for ($i = 0; $i <= $m; $i++) {   
-		    for ($j = 0; $j <= $n; $j++) {  
-			      if ($i == 0)  
-				        $dp[$i][$j] = $j ;
-  			    else if($j == 0)  
-	  			      $dp[$i][$j] = $i;
-		  	    else if($str1[$i - 1] == $str2[$j - 1])  
-			  	      $dp[$i][$j] = $dp[$i - 1][$j - 1]; 
-			      else {  
-				        $dp[$i][$j] = 1 + min($dp[$i][$j - 1], // Insert  
-					             				    $dp[$i - 1][$j], // Delete  
-							  		              $dp[$i - 1][$j - 1]); // Replace  
-			       } 
-		    } 
-	  } 
-	
-	  return $dp[$m][$n] ; 
+	// dp[][] in bottom up manner  - Dynamic programming memoization
+	for ($i = 0; $i <= $m; $i++) {   
+		for ($j = 0; $j <= $n; $j++) {  
+			if ($i == 0)  
+				$dp[$i][$j] = $j ;
+			else if($j == 0)  
+				$dp[$i][$j] = $i;
+			else if($str1[$i - 1] == $str2[$j - 1])  
+				$dp[$i][$j] = $dp[$i - 1][$j - 1]; 
+			else {  
+				$dp[$i][$j] = 1 + min($dp[$i][$j - 1], // Insert  
+									$dp[$i - 1][$j], // Delete  
+									$dp[$i - 1][$j - 1]); // Replace  
+			} 
+		} 
+	} 
+
+	return $dp[$m][$n] ; 
 } 
 
 // minDistance($str1, $str2, strlen($str1), strlen($str2)); 
