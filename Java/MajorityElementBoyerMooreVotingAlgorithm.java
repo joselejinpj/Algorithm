1a7ba96a166cc1d1a2eb2ba4e9a84ddc9854bdf6
joselejinpj <67502523+joselejinpj@users.noreply.github.com>
2020-08-13 00:11:54 +0530

    Update MajorityElementBoyerMooreVotingAlgorithm.java

--- Java/MajorityElementBoyerMooreVotingAlgorithm.java
+++ Java/MajorityElementBoyerMooreVotingAlgorithm.java
@@ -1,7 +1,7 @@
 /*****
 
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-Find solution in O(n)
+Find solution in O(n).
 
 -----------------
 Sample Test Cases
@@ -33,3 +33,4 @@ class Solution {
         return majorEle;
     }
 }
+ 
