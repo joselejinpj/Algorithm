24b18aaac97c193c3565cd1d0a313dc3e5fd1fb6
joselejinpj <67502523+joselejinpj@users.noreply.github.com>
2020-09-05 21:49:16 +0530

    Create FirstUniqueCharacterInString.py

---
+++ Python/FirstUniqueCharacterInString.py
@@ -0,0 +1,24 @@
+"""
+
+Given a string find the first non-repeating character in it and return its index. If it doesn't exist return -1.
+
+-----------------
+Sample Test Cases
+-----------------
+Test Case 1:
+
+Input: "leetcode"
+Output: 0
+
+"""
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = collections.Counter(s)
+        
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx  
+            
+        return -1
+        
